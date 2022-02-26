4c3de51a3323f50de7271780e3b1148da5442b61
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-02-26 13:33:25 +0100

    Complete project

--- odin-bubble-sort.rb
+++ odin-bubble-sort.rb
@@ -1,11 +1,22 @@
 def bubble_sort(array)
   #magic code
-  array.each_with_index do |number, i|
-    unless array[i + 1] == nil
-      if number > array[i + 1]
-        temp = array[i + 1]
-        array[i + 1] = number
-        array[i] = temp
+is_sorted = false
+  until is_sorted
+    array.each_with_index do |number, i|
+      unless array[i + 1] == nil
+        if number > array[i + 1]
+          temp = array[i + 1]
+          array[i + 1] = number
+          array[i] = temp
+        end
+      end
+    end
+    for i in (0...(array.length - 1))
+      if array[i] > array[i + 1]
+        is_sorted = false
+        break
+      else
+        is_sorted = true
       end
     end
   end
