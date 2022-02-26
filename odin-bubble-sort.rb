d78f3779ba90c2ee7e3fc578608e451b6355ec39
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-02-26 13:10:09 +0100

    Implement method for one iteration

--- odin-bubble-sort.rb
+++ odin-bubble-sort.rb
@@ -1,5 +1,15 @@
 def bubble_sort(array)
   #magic code
+  array.each_with_index do |number, i|
+    unless array[i + 1] == nil
+      if number > array[i + 1]
+        temp = array[i + 1]
+        array[i + 1] = number
+        array[i] = temp
+      end
+    end
+  end
+  array
 end
 
 # Test
